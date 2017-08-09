c58ebdc0354a86e5d7d1309e211743f6264fdac1
Anderson de Oliveira <andersonba@me.com>
2017-08-09 01:19:34 -0300

    examples: add socket.io

---
+++ examples/socket.io/index.js
@@ -0,0 +1,54 @@
+import http from 'http';
+import express from 'express';
+import SocketIO from 'socket.io';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+import yaml from 'js-yaml';
+import YveBot from '../../src/core';
+
+const app = express();
+const server = http.Server(app);
+const io = SocketIO(server);
+
+app.use(bodyParser.json());
+
+const example = yaml.safeLoad(fs.readFileSync(__dirname + '/../example.eyaml', 'utf8'));
+const bot = new YveBot(example);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', chat => {
+  const sessionId = chat.id;
+
+  bot
+  .on('typing', () => io.to(sessionId).emit('is typing'))
+  .on('typed', () => io.to(sessionId).emit('is typed'))
+  .on('talk', message => {
+    io.to(sessionId).emit('receive message', {
+      from: 'BOT',
+      message,
+    });
+  })
+  .on('end', () => chat.disconnect());
+
+  chat
+    .on('join', () => {
+      chat.emit('connected', sessionId);
+      chat.join(sessionId);
+      bot.session(sessionId).start();
+    })
+    .on('send message', ({ user, message }) => {
+      io.to(user).emit('receive message', {
+        message,
+        from: 'USER',
+      });
+
+      bot.session(sessionId).hear(message);
+    });
+});
+
+server.listen(3000, () => {
+  console.log('Yve server example listening on port 3000');
+});

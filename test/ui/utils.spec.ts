6809d5b74d69cad15295401e0856315fa0078b0c
Anderson de Oliveira <andersonba@me.com>
2017-09-07 22:59:04 -0300

    tests: add ui/utils spec

---
+++ test/ui/utils.spec.ts
@@ -0,0 +1,17 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import * as utils from '../../src/ui/utils';
+
+describe('utils', () => {
+  describe('arrayToString', () => {
+    it('common', () => {
+      function testMethod(arr, sep, expected) {
+        expect(utils.arrayToString(arr, sep)).to.be.equal(expected);
+      }
+
+      testMethod(['one', 'two', 'three'], 'and', 'one, two and three');
+      testMethod(['one', 'two'], 'or', 'one or two');
+      testMethod(['one'], 'or', 'one');
+    });
+  });
+});

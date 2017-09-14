fd84c909dd4e707661640ea12faa5454f8a2fbb8
Anderson de Oliveira <andersonba@me.com>
2017-09-14 13:13:58 -0300

    fix optional types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
 import { YveBot } from './core/bot';
 
 export interface YveBotOptions {
-  enableWaitForSleep: boolean,
-  rule: Rule,
+  enableWaitForSleep?: boolean,
+  rule?: Rule,
 }
 
 export interface Rule {
--- src/ui/types.ts
+++ src/ui/types.ts
@@ -2,13 +2,13 @@ import { YveBotOptions } from '../types';
 export type MessageSource = 'BOT' | 'USER';
 
 export interface ChatOptions {
-  target: string;
-  inputPlaceholder: string;
-  inputPlaceholderSingleChoice: string;
-  inputPlaceholderMutipleChoice: string;
-  doneMultipleChoiceLabel: string;
-  andSeparatorText: string;
-  submitLabel: string;
-  autoFocus: boolean;
+  target?: string;
+  inputPlaceholder?: string;
+  inputPlaceholderSingleChoice?: string;
+  inputPlaceholderMutipleChoice?: string;
+  doneMultipleChoiceLabel?: string;
+  andSeparatorText?: string;
+  submitLabel?: string;
+  autoFocus?: boolean;
   yveBotOptions?: YveBotOptions;
 }

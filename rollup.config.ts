3230d9938836268ee9eae4478f6229e7ee5f44e0
Anderson de Oliveira <andersonba@me.com>
2017-11-14 03:58:02 -0300

    split types in optional modules

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,7 @@
+import path from 'path';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
+import multiEntry from 'rollup-plugin-multi-entry';
 import resolve from 'rollup-plugin-node-resolve';
 import uglify from 'rollup-plugin-uglify';
 
@@ -11,10 +13,6 @@ const server = {
     resolve(),
     json(),
     uglify(),
-  ],
-
-  external: [
-    'isomorphic-unfetch',
   ],
 
   output: {
@@ -33,7 +31,6 @@ const client = {
     commonjs(),
     resolve({
       browser: true,
-      preferBuiltins: false,
     }),
     json(),
     uglify(),
@@ -46,7 +43,41 @@ const client = {
   },
 };
 
+const typeExtensions = ['string-search'].map((eType) => {
+  const src = path.resolve('./compiled/core/index.js');
+  return {
+    input: `compiled/ext/types/${eType}.js`,
+
+    plugins: [
+      commonjs(),
+      resolve({
+        browser: true,
+      }),
+      json(),
+      multiEntry({ exports: false }),
+    ],
+
+    external: [
+      src,
+      'isomorphic-unfetch',
+    ],
+
+    globals: {
+      [src]: 'YveBot',
+    },
+
+    output: {
+      file: `lib/ext/types/${eType}.js`,
+      format: 'umd',
+      paths: {
+        [src]: '../../core',
+      },
+    },
+  };
+});
+
 export default [
   client,
   server,
+  ...typeExtensions,
 ];

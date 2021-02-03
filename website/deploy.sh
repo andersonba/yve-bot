3e65fbcf4034d3d09a4e8728505cbc406661b9f1
Anderson de Oliveira <andersonba@me.com>
2021-02-03 03:55:38 -0300

    update travis file

--- website/deploy.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-git config --global user.name "${GH_NAME}"
-git config --global user.email "${GH_EMAIL}"
-echo "machine github.com login ${GH_NAME} password ${GH_TOKEN}" > ~/.netrc
-cd website
-rm -rf build
-yarn install
-yarn build
-GIT_USER="${GH_NAME}" yarn publish-gh-pages

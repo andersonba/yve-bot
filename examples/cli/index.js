c4a3a572d414ab4cd67cbe81d779d7c82c4a4d6c
Tiago Silva <31517712+tiagoemsi@users.noreply.github.com>
2025-02-07 10:28:59 -0300

    refactor: standardize string quotes and improve code readability in CLI example

--- examples/cli/index.js
+++ examples/cli/index.js
@@ -1,30 +1,32 @@
-const fs = require('fs');
-const yaml = require('js-yaml');
-const prompt = require('prompt');
-const YveBot = require('../../lib/core');
+const fs = require("fs");
+const yaml = require("js-yaml");
+const prompt = require("prompt");
+const YveBot = require("../../lib/core");
 
-const example = yaml.load(fs.readFileSync(__dirname + '/../chat.yaml', 'utf8'));
+const example = yaml.load(fs.readFileSync(__dirname + "/../chat.yaml", "utf8"));
 const bot = new YveBot(example);
 
-prompt.message = '';
+prompt.message = "";
 prompt.start();
 
 bot
-  .on('talk', (message, data) => {
+  .on("talk", (message, data) => {
     console.log(message);
 
     if (data.options && data.options.length) {
-      const options = data.options.map(o => o.value || o.label);
-      console.log(`Choose an option: [${options.join(', ')}]`);
+      const options = data.options.map((o) => o.value || o.label);
+      console.log(`Choose an option: [${options.join(", ")}]`);
     }
   })
-  .on('hear', () => {
-    prompt.get('You', (err, res) => {
-      if (err) { throw err; }
+  .on("hear", () => {
+    prompt.get("You", (err, res) => {
+      if (err) {
+        throw err;
+      }
       bot.hear(res.You);
     });
   })
-  .on('end', data => {
-    console.log('> Finished with data:', data);
+  .on("end", (data) => {
+    console.log("> Finished with data:", data);
   })
   .start();
